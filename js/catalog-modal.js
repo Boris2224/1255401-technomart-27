0755a21f66e936c67b947ea22bbc3be63e190f41
Britva906090 <boris.kozhevnikov90@gmail.com>
2020-03-04 23:28:49 +0300

    js

---
+++ js/catalog-modal.js
@@ -0,0 +1,27 @@
+ /*модальное окно : добавление в корзину*/
+ 
+var buyLink = document.querySelectorAll(".buy-button");
+  var succesPopup = document.querySelectorAll(".modal-cart-success");
+  var succesClose = document.querySelectorAll(".modal-close");
+  var continueBuy = document.querySelectorAll(".continue-button");
+
+  for(var i = 0; i < buyLink.length; i++){
+    buyLink[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      succesPopup[0].classList.add("modal-show");
+    });
+  };
+
+  for(var i = 0; i < succesClose.length; i++){
+    succesClose[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      succesPopup[0].classList.remove("modal-show");
+    });
+  };
+
+  for(var i = 0; i < continueBuy.length; i++){
+    continueBuy[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      succesPopup[0].classList.remove("modal-show");
+    });
+  };
---
+++ js/index-modal.js
@@ -0,0 +1,108 @@
+/*модальное окно : напишите нам */
+var link = document.querySelector(".write-us-link");
+var popup = document.querySelector(".modal-write-us-window");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector(".write-us-form");
+var login = popup.querySelector("[name=name]");
+var email = popup.querySelector("[name=email]");
+var textarea = popup.querySelector("[name=textarea]");
+
+try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+
+    if (storage) {
+        login.value = storage;
+      } else {
+    login.focus();
+       }
+});
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value || !textarea.value) {
+        evt.preventDefault();
+        popup.classList.remove("modal-error");
+        popup.offsetWidth = popup.offsetWidth;
+        popup.classList.add("modal-error");
+    } else {
+        if (isStorageSupport) {
+            localStorage.setItem("email", email.value);
+          }
+      }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
+
+/*модальное окно : карта */
+
+  var mapLink = document.querySelector(".mini-map-link");
+
+  var mapPopup = document.querySelector(".modal-map-window");
+  var mapClose = mapPopup.querySelector(".modal-close");
+
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        evt.preventDefault();
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
+
+  /*модальное окно : добавление в корзину*/
+
+  var buyLink = document.querySelectorAll(".buy-button");
+  var succesPopup = document.querySelectorAll(".modal-cart-success");
+  var succesClose = document.querySelectorAll(".modal-close");
+  var continueBuy = document.querySelectorAll(".continue-button");
+
+  for(var i = 0; i < buyLink.length; i++){
+    buyLink[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      succesPopup[0].classList.add("modal-show");
+    });
+  };
+
+  for(var i = 0; i < succesClose.length; i++){
+    succesClose[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      succesPopup[0].classList.remove("modal-show");
+    });
+  };
+
+  for(var i = 0; i < continueBuy.length; i++){
+    continueBuy[i].addEventListener("click", function(event) {
+      event.preventDefault();
+      succesPopup[0].classList.remove("modal-show");
+    });
+  };
